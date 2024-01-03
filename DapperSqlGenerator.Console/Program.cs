ecdf88802032f6dbf6728fa3d29ba788024ecf8e
David Zenou <david.zenou@flowbank.com>
2024-01-03 15:02:56 +0100

    begin stored Procedure

--- DapperSqlGenerator.Console/Program.cs
+++ DapperSqlGenerator.Console/Program.cs
@@ -35,18 +35,28 @@ namespace DapperSqlGenerator.Console
 
             constantsDir = helpersDir = registerServiceExtensionDir = configurationDir = cacheServiceDir = dataServiceDir = dataRepositoryDir = dataModelDir = @"C:\proj_net\testGenerator\ConsoleApp1";
 
+            //if includeOnlyTables is not empty , let excludedTables empty it will be computed afted
+            //else if includeOnlyTables is empty , you can fill excludedTables
+            string[] includeOnlyTables = {  };
             //excludes table we don't want ot generate
-            string[] excludedTables = { "EFMigrationsHistory", "AspNetRoleClaims", "AspNetRoles", "AspNetUserClaims", "AspNetUserLogins", "AspNetUserRoles", "AspNetUsers", "AspNetUserTokens" };
+            string[] excludedTables = {  };
+            //string[] excludedTables = { "EFMigrationsHistory", "AspNetRoleClaims", "AspNetRoles", "AspNetUserClaims", "AspNetUserLogins", "AspNetUserRoles", "AspNetUsers", "AspNetUserTokens" };
+
             //references table (static tables)
             string[] refTables = { "Certification", "CountryCompany", "Department", "Lang" , "JobOfferLevel" };
 
-           
-            string dataModelNamespace = $"{projectName}.Model";
-            string dataRepostioryNamespace = $"{projectName}.Repositories";
-            string dataServiceNamespace = $"{projectName}.Services";
-
+            string dataModelNamespace       = $"{projectName}.Model";
+            string dataRepostioryNamespace  = $"{projectName}.Repositories";
+            string dataServiceNamespace     = $"{projectName}.Services";
 
             var model = TSqlModelFactory.CreateModel(connectionString);
+
+            if (includeOnlyTables.Any())
+            {
+                var  alltables = model.GetAllTables().Select(t => t.Name.Parts[1].PascalCase()).ToArray();
+                excludedTables = alltables.Except(includeOnlyTables).ToArray();
+            }
+             
 
             List<IGeneratorService> generatorServices = new List<IGeneratorService>();
             generatorServices.Add( new DataModelGeneratorService(dataModelNamespace, dataModelDir, excludedTables));
---
+++ DapperSqlGenerator.Console/Services/StoredProcedureGeneratorService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.SqlServer.Dac.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperSqlGenerator.Console.Services
+{
+    public class StoredProcedureGeneratorService : IGeneratorService
+    {
+        string projectName;
+        string dataModelNamespace;
+        string serviceModelNamespace;
+        string dataRepositoryNamespace;
+        string dirToWrite;
+        string[] excludedTables;
+        string[] refTables;
+
+        public List<string> Warnings
+        {
+            get
+            {
+                return new List<string>();
+            }
+        }
+
+        public StoredProcedureGeneratorService(string serviceModelNamespace, string dataModelNamespace, string dataRepositoryNamespace, string dirToWrite, string projectName, string[] excludedTables, string[] refTables)
+        {
+            this.serviceModelNamespace = serviceModelNamespace;
+            this.dirToWrite = dirToWrite;
+            this.excludedTables = excludedTables;
+            this.refTables = refTables;
+            this.dataModelNamespace = dataModelNamespace;
+            this.dataRepositoryNamespace = dataRepositoryNamespace;
+            this.projectName = projectName;
+        }
+
+        public async Task GenerateFilesAsync(TSqlModel model)
+        {
+            //stored Procedures
+            var objs = model.GetObjects(DacQueryScopes.All, Procedure.TypeClass).ToList();
+            foreach (var proc in objs)
+            {
+                var name = proc.Name;
+                foreach (var parameter in proc.GetChildren().Where(child => child.ObjectType.Name == "Parameter"))
+                {
+                    var dataType = parameter.GetReferenced(Parameter.DataType).First(); // Obtiene el tipo del parametro.
+                    var parameterName = parameter.Name.Parts.Last();
+                }
+            }
+            //stored Procedures
+        }
+
+
+
+    }
+}
